909781dad9232606b2ed2c770e61067d9f8c20c8
alka0 <alka016kumari@gmail.com>
2021-06-01 18:24:06 +0530

    //Second commit

---
+++ mvn-ecl-jv-spring/src/main/java/com/cts/javaconfig/AppConfig.java
@@ -0,0 +1,13 @@
+package com.cts.javaconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+	@Bean
+	public HelloWorld getBean(){
+		return new HelloWorldImpl();
+	}
+
+}
